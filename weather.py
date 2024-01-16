d8c41ae0a4cc570f4b6013a1e1c95ef30b412472
Blanka <csgb@zs2.hu>
2024-01-16 15:27:28 +0100

    Part 3 finished, final result saved:D

--- weather.py
+++ weather.py
@@ -30,8 +30,6 @@ total_monthly_precipitation_seattle=[] #turn the values from the dictionary into
 for month in dict_monthly_precipitation:
     total_monthly_precipitation_seattle.append(dict_monthly_precipitation[month])
 
-print(f'The total precipitation in each month in Seattle in 2010 was {total_monthly_precipitation_seattle}')
-
 Cities = {
     "Seattle" : {
         "station":"GHCND:US1WAKG0038",
@@ -40,27 +38,68 @@ Cities = {
     },
 }
 
-with open('results.json', 'w', encoding='utf-8') as file:
-     json.dump(Cities, file)
-
 #Part 2
 #2.1 Calculate total yearly precipitation
 total_yearly_precipitation_seattle=0
 for month in total_monthly_precipitation_seattle:
     total_yearly_precipitation_seattle=total_yearly_precipitation_seattle+month
 
-print(f'The total yearly precipitation in 2010 in Seattle was {total_yearly_precipitation_seattle}')
-
 #2.2 Calculate relative monthly precipitation
 relative_monthly_precipitation_seattle=[]
 for month in total_monthly_precipitation_seattle:
     relative_monthly_precipitation_seattle.append(float(month)/float(total_yearly_precipitation_seattle))
 
-print(f'The relative precipitation per month in Seattle in 2010 was {relative_monthly_precipitation_seattle}')
-
 #add the new info to the dictionary
 Cities["Seattle"]["total_yearly_precipitation"]=total_yearly_precipitation_seattle 
 Cities["Seattle"]["relative_monthly_precipitation"]=relative_monthly_precipitation_seattle
 
+#Part 3: extend code for all locations
+WeatherInfo={} #create a dictionary to store all my data in
+#select measurement for given location
+for location in stations:
+    station_measurements=[]
+   #something here about adding to dict?
+    for measurement in precipitation:
+        if measurement["station"]==location["Station"]:
+            station_measurements.append(measurement)
+    #1.2 calculate total monthly precipitation for given location
+    dict_monthly_precipitation={} #create a dictionary for total monthly precipitation, with year and month as key
+    for measurement in station_measurements:
+        for month in ['2010-01', '2010-02', '2010-03', '2010-04', '2010-05', '2010-06', '2010-07', '2010-08', '2010-09', '2010-10', '2010-11', '2010-12']:
+            if measurement["date"].startswith(month):
+                if month not in dict_monthly_precipitation:
+                    dict_monthly_precipitation[month]=measurement["value"]
+                if month in dict_monthly_precipitation:
+                    dict_monthly_precipitation[month]=float(dict_monthly_precipitation[month])+float(measurement["value"])
+    total_monthly_precipitation=[] #turn the values from the dictionary into a list
+    for month in dict_monthly_precipitation:
+        total_monthly_precipitation.append(dict_monthly_precipitation[month])
+    #calculate total yearly precipitation in given location
+    total_yearly_precipitation=0
+    for month in total_monthly_precipitation:
+        total_yearly_precipitation=total_yearly_precipitation+month
+    #calculate relative monthly precipitation for given location
+    relative_monthly_precipitation=[]
+    for month in total_monthly_precipitation:
+        relative_monthly_precipitation.append(float(month)/float(total_yearly_precipitation))
+    #add results to the WeatherInfo dictionary
+    WeatherInfo[location["Location"]] = {
+            "station":location["Station"],
+            "state":location["State"],
+            "total_monthly_precipitation":total_monthly_precipitation,
+            "total_yearly_precipitation": total_yearly_precipitation,
+            "relative_monthly_precipitation":relative_monthly_precipitation
+        }
+
+#calculate relative yearly precipitation
+totalrainfall=0 #first calculate total rainfall everywhere
+for city in WeatherInfo:
+    totalrainfall=totalrainfall+WeatherInfo[city]["total_yearly_precipitation"]
+
+for city in WeatherInfo:
+    relative_yearly_precipitation=(float(WeatherInfo[city]["total_yearly_precipitation"])/float(totalrainfall))
+    WeatherInfo[city]["relative_yearly_precipitation"]=relative_yearly_precipitation
+
+#save final result as json file (35.8% of rain fell in Seattle)
 with open('results.json', 'w', encoding='utf-8') as file:
-     json.dump(Cities, file)
+     json.dump(WeatherInfo, file)
